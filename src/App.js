5fb53058a26e2b6731a3f9306c90110533c927cd
najjarzahra <zahranajjar011@gmail.com>
2023-05-27 13:30:35 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Welcome from './welcome';
+
+function App() {
+  return (
+    <div>
+      <Welcome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map from './Map';
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>hello , this is my maps!</h1>
+      <Map/>
+    </div>
+  );
+};
+
+export default Welcome;
